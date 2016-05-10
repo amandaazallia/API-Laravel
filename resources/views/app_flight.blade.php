6178f88994bb9fdc1113541448f6d1dd2f72580f
amandaazallia <mandazal123@gmail.com>
2016-05-10 11:11:36 +0700

    update

--- resources/views/app_flight.blade.php
+++ resources/views/app_flight.blade.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
-<head lang="en">
-  <body>
-    <meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="{{ url('http://fonts.googleapis.com/icon?family=Material+Icons') }}" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="{{ url('css/materialize.min.css') }} "  media="screen,projection"/>
 
-    <meta name="keywords" content="HTML, CSS, JS, JavaScript, framework, metro, front-end, frontend, web development">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+        <nav>
+          <div class="nav-wrapper">
+            <a href="{{ url('/') }}" class="brand-logo left">Logo </a>
+            <ul class="right hide-on-med-and-down">
+              <li><a href="{{ url('master/currency') }}">Currency</a></li>
+              <li><a href="{{ url('master/country') }}">Country</a></li>
+              <li><a href="{{ url('master/lang') }}">Language</a></li>
+              <li><a href="{{ url('master/airport') }}">Airport</a></li>
+
+            </ul>
+          </div>
+        </nav>
+
+      <div class="container">
+    @yield('content')
+    </div>
+
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="{{ url('https://code.jquery.com/jquery-2.1.1.min.js') }}"></script>
+      <script type="text/javascript" src="{{ url('js/materialize.min.js') }}"></script>
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+      <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
 
 
-    <title>API Airline</title>
+      @yield('footer')
 
-    <link href="{{ url('css/materialize.css') }}" rel="stylesheet">
-    <link href="{{ url('css/materialize.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-
-
-    <script src="{{ url('js/materialize.min.js') }}"></script>
-    <script src="{{ url('js/materilaize.js') }}"></script>
-
-@yield('content')
-<!-- 
-@yield('footer') -->
-
-</body>
-</head>
-</html>
+    </body>
+  </html>
